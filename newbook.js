ab3f1c6c69b284e4aed17c1353b29c2f7b6fbb11
Lior Levy <lior7212@gmail.com>
2017-02-01 13:43:22 -0800

    add home page and our vision page

---
+++ newbook.js
@@ -0,0 +1,23 @@
+document.getElementById("geor").onclick = function(){
+
+	location.href = "http://courses.washington.edu/dtcg/";
+}
+
+document.getElementById("svo").onclick = function(){
+
+	location.href = "http://depts.washington.edu/svobodad/";
+}
+
+document.getElementById("emma").onclick = function(){
+
+	location.href = "http://www.emmabandrews.org/project/";
+}
+
+document.getElementById("sultan").onclick = function(){
+
+	location.href = "http://depts.washington.edu/ndtwp/sultansdreams/";
+}
+
+
+
+			
